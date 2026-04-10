61b12c6e7c505dfb7527a1b276c512382a423502
Nicholas <darthshroda28@gmail.com>
2026-04-10 14:46:22 +0800

    Added Patrick's

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import HeroAboutUs from "@/src/components/marketing-page/HeroAboutUs/HeroAboutUs";
 import HeroWelcome from "@/src/components/marketing-page/HeroWelcome";
+import HerousedByStudents from "@/src/components/marketing-page/HeroUsedByStudents/HeroUsedByStudents";
+import InfiniteCarousel from "@/src/components/marketing-page/HeroUsedByStudents/InfiniteCarousell";
 
 export default function page() {
   return (
     <>
       <HeroWelcome />
       <HeroAboutUs />
+      <HerousedByStudents />
     </>
   );
 }
