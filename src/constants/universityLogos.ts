8ae16fe4ef46f1b75c57529ad8f9232aa753b9ad
IslandGate <patrick.xu@online.island.edu.hk>
2026-04-14 22:14:43 +0800

    UniversityLogos.ts

--- src/constants/universityLogos.ts
+++ src/constants/universityLogos.ts
@@ -1,3 +1,4 @@
+// UniversityLogos.ts
 export interface UniversityLogo {
   id: string;
   name: string;
@@ -8,41 +9,41 @@ export const UNIVERSITY_LOGOS: UniversityLogo[] = [
   {
     id: "harvard",
     name: "Harvard University",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/Harvard.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/Harvard.svg",
   },
   {
     id: "yale",
     name: "Yale University",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/Columbia.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/Yale.svg",
   },
   {
     id: "stanford",
     name: "Stanford University",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/Cambridge.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/Stanford.svg",
   },
   {
     id: "mit",
     name: "MIT",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/UChicago.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/MIT.svg",
   },
   {
     id: "uchicago",
     name: "University of Chicago",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/LSE.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/UChicago.svg",
   },
   {
-    id: "uchicago2",
+    id: "umich",
     name: "University of Michigan",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/UMich.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/UMich.svg",
   },
   {
     id: "hkust",
     name: "HKUST",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/HKUST.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/HKUST.svg",
   },
   {
     id: "lse",
     name: "LSE",
-    path: "/marketing-page-assets/HeroUsedByStudends-assets/UniversityLogos/LSE.svg",
+    path: "/marketing-page-assets/HeroUsedByStudents-assets/UniversityLogos/LSE.svg",
   },
 ];
